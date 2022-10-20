4802d5d178b075bd61c0a08c9bbc398d0e8fc74f
Chriss99 school PC <chrissblock99@gmail.com>
2022-10-20 11:12:35 +0200

    probably fixing the setup of playerData

--- src/main/java/me/chriss99/spellbend/playerdata/PlayerDataUtil.java
+++ src/main/java/me/chriss99/spellbend/playerdata/PlayerDataUtil.java
@@ -1,5 +1,8 @@
 package me.chriss99.spellbend.playerdata;
 
+import com.google.gson.Gson;
+import me.chriss99.spellbend.SpellBend;
+import me.chriss99.spellbend.data.CoolDownEntry;
 import me.chriss99.spellbend.harddata.PersistentDataKeys;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -7,7 +10,11 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+
 public class PlayerDataUtil {
+    private static final Gson gson = SpellBend.getGson();
+
     /**Sets up all the PersistentData of the player
      *
      * @param player The player who's PersistentData to set up
@@ -17,8 +24,8 @@ public class PlayerDataUtil {
         //data.set(PersistentDataKeys.gemsKey, PersistentDataType.INTEGER, 150);
         //data.set(PersistentDataKeys.goldKey, PersistentDataType.INTEGER, 650);
         //data.set(PersistentDataKeys.spellsOwnedKey, PersistentDataType.INTEGER_ARRAY, new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
-        data.set(PersistentDataKeys.coolDownsKey, PersistentDataType.STRING, "");
-        data.set(PersistentDataKeys.dmgModsKey, PersistentDataType.STRING, "1, 1, 1");
+        data.set(PersistentDataKeys.coolDownsKey, PersistentDataType.STRING, gson.toJson(new HashMap<String, CoolDownEntry>()));
+        data.set(PersistentDataKeys.dmgModsKey, PersistentDataType.STRING, gson.toJson(new float[3]));
         //data.set(PersistentDataKeys.crystalsKey, PersistentDataType.INTEGER, 0);
         //data.set(PersistentDataKeys.crystalShardsKey, PersistentDataType.INTEGER, 0);
     }
@@ -55,13 +62,13 @@ public class PlayerDataUtil {
         try {
             CoolDowns.loadCoolDowns(player);
         } catch (Exception e) {
-            Bukkit.getLogger().warning("The loading of " + player.getDisplayName() + "'s Cooldowns generated an exception!");
+            Bukkit.getLogger().warning("The loading of " + player.displayName() + "'s Cooldowns generated an exception!");
             e.printStackTrace();
         }
         try {
             DmgMods.loadDmgMods(player);
         } catch (Exception e) {
-            Bukkit.getLogger().warning("The loading of " + player.getDisplayName() + "'s DmgMods generated an exception!");
+            Bukkit.getLogger().warning("The loading of " + player.displayName() + "'s DmgMods generated an exception!");
             e.printStackTrace();
         }
         /*try {
@@ -110,13 +117,13 @@ public class PlayerDataUtil {
         try {
             CoolDowns.saveCoolDowns(player);
         } catch (Exception e) {
-            Bukkit.getLogger().warning("The saving of " + player.getDisplayName() + "'s CoolDowns generated an exception!");
+            Bukkit.getLogger().warning("The saving of " + player.displayName() + "'s CoolDowns generated an exception!");
             e.printStackTrace();
         }
         try {
             DmgMods.saveDmgMods(player);
         } catch (Exception e) {
-            Bukkit.getLogger().warning("The saving of " + player.getDisplayName() + "'s DmgMods generated an exception!");
+            Bukkit.getLogger().warning("The saving of " + player.displayName() + "'s DmgMods generated an exception!");
             e.printStackTrace();
         }
         /*try {
