c88c232e6bc341046eac10ea83615a38a2ff174e
chriss99 <chrissblock99@gmail.com>
2022-12-28 00:54:58 +0100

    added sound to Escape_Through_Time

--- src/main/java/me/chriss99/spellbend/spells/Escape_Through_Time.java
+++ src/main/java/me/chriss99/spellbend/spells/Escape_Through_Time.java
@@ -8,6 +8,7 @@ import me.chriss99.spellbend.harddata.Colors;
 import me.chriss99.spellbend.harddata.Enums;
 import me.chriss99.spellbend.util.PlayerUtil;
 import me.chriss99.spellbend.util.math.MathUtil;
+import net.kyori.adventure.sound.SoundStop;
 import org.bukkit.*;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
@@ -51,10 +52,15 @@ public class Escape_Through_Time extends Spell implements Killable {
     }
 
     private void setupArmorStand() {
-        armorStand = caster.getWorld().spawn(armorStandOrigin, ArmorStand.class);
+        World world = caster.getWorld();
+        armorStand = world.spawn(armorStandOrigin, ArmorStand.class);
         armorStand.setVisible(false);
         armorStand.setBasePlate(false);
         armorStand.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 99999, 255, false, false));
+
+        world.playSound(armorStandOrigin, Sound.ENTITY_ILLUSIONER_MIRROR_MOVE, 3f, 1.2f);
+        world.playSound(armorStandOrigin, Sound.ENTITY_ENDERMAN_TELEPORT, 3f, 1.2f);
+        world.playSound(armorStandOrigin, Sound.BLOCK_BEACON_ACTIVATE, 3f, 1.2f);
 
         for (int i = 0; i < 360; i += 10) {
             Color color = Colors.getRandomYellow1or2();
@@ -93,12 +99,19 @@ public class Escape_Through_Time extends Spell implements Killable {
 
             @Override
             public void run() {
-                caster.teleport(new Location(caster.getWorld(), 0, 0, 0, casterOrigin.getYaw(), casterOrigin.getPitch()).add(MathUtil.lerpVector(casterOrigin.toVector(), armorStandOrigin.toVector(), Math.pow(time/20f, 0.333333333))));
-                caster.getWorld().spawnParticle(Particle.SMOKE_LARGE, caster.getLocation().add(0 , 1.3, 0), 5, 0, 0, 0, 0.5);
-                caster.getWorld().spawnParticle(Particle.VILLAGER_ANGRY, caster.getLocation().add(0 , 0.8, 0), 5, 0, 0, 0, 0);
+                World world = caster.getWorld();
+                Location location = caster.getLocation();
+                caster.teleport(new Location(world, 0, 0, 0, casterOrigin.getYaw(), casterOrigin.getPitch()).add(MathUtil.lerpVector(casterOrigin.toVector(), armorStandOrigin.toVector(), Math.pow(time/20f, 0.333333333))));
+                world.spawnParticle(Particle.SMOKE_LARGE, location.add(0 , 1.3, 0), 5, 0, 0, 0, 0.5);
+                world.spawnParticle(Particle.VILLAGER_ANGRY, location.add(0 , 0.8, 0), 5, 0, 0, 0, 0);
+                world.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT, 2f, time/10f);
+                world.playSound(location, Sound.ENTITY_ILLUSIONER_PREPARE_MIRROR, 2f, time/10f);
 
                 if (time == 0) {
                     armorStand.remove();
+                    world.stopSound(SoundStop.named(Sound.ENTITY_ILLUSIONER_PREPARE_MIRROR));
+                    world.stopSound(SoundStop.named(Sound.ENTITY_ENDERMAN_TELEPORT));
+                    world.playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 2, 1);
 
                     escapeTask.cancel();
                     armorStandTask.cancel();
@@ -111,17 +124,28 @@ public class Escape_Through_Time extends Spell implements Killable {
     }
 
     private void explode() {
+        World world = caster.getWorld();
         Map<Player, Double> players = PlayerUtil.getPlayersNearLocation(armorStandOrigin, 4.5);
         players.remove(caster);
-        for (Map.Entry<Player, Double> entry : players.entrySet())
-            PlayerSessionData.getPlayerSession(entry.getKey()).getHealth().damagePlayer(caster, 3, item);
+        for (Map.Entry<Player, Double> entry : players.entrySet()) {
+            Player player = entry.getKey();
+            PlayerSessionData sessionData = PlayerSessionData.getPlayerSession(player);
+            sessionData.getHealth().damagePlayer(caster, 3, item);
+            sessionData.getSpellHandler().stunPlayer(20);
+            world.spawnParticle(Particle.FLASH, player.getLocation(), 1, 0, 0, 0, 0);
+            //player.moveUp(1);
+            //player.moveAway(1);
+        }
 
-        armorStandOrigin.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, armorStandOrigin.add(0, 1, 0), 1);
+        world.spawnParticle(Particle.EXPLOSION_LARGE, armorStandOrigin.add(0, 1.3, 0), 1, 0, 0, 0, 0);
+        world.spawnParticle(Particle.VILLAGER_ANGRY, armorStandOrigin.add(0, 0.8, 0), 1, 0, 0, 0, 0);
+        world.playSound(armorStandOrigin, Sound.ENTITY_GENERIC_EXPLODE, 2f, 1f);
         armorStand.remove();
     }
 
     @Override
     public void casterDeath(@Nullable Entity Killer) {
+        explode();
         cancelSpell();
     }
 
@@ -138,6 +162,7 @@ public class Escape_Through_Time extends Spell implements Killable {
         else Bukkit.getLogger().warning(caster.getName() + " had Escape Through Time Active but no corresponding CoolDown of type " + spellType + " was found!");
 
         spellHandler.removeClickableSpellRunnable(item);
+        armorStand.remove();
         armorStandTask.cancel();
         if (escapeTask != null)
             escapeTask.cancel();
