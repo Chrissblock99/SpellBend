ce1f1fc6034c404b0e5d31d0eb484789d9ad4145
chriss99 <chrissblock99@gmail.com>
2023-03-10 20:25:40 +0100

    added InventoryUtil

---
+++ src/main/java/me/chriss99/spellbend/util/InventoryUtil.java
@@ -0,0 +1,40 @@
+package me.chriss99.spellbend.util;
+
+import me.chriss99.spellbend.data.SpellHandler;
+import me.chriss99.spellbend.harddata.PersistentDataKeys;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class InventoryUtil {
+    /**
+     *
+     * @param inv The inventory to search for Spells inside
+     * @return The amount of Spells inside the inventory
+     */
+    public static int spellsInsideInventory(@NotNull Inventory inv) {
+        int num = 0;
+        for (ItemStack item : inv.getContents())
+            if (SpellHandler.itemIsSpell(item))
+                num++;
+        return num;
+    }
+
+    /**
+     *
+     * @param inv The inventory to search for SpellName
+     * @param spellName The spellName to check for
+     * @return The amount of Spells inside the inventory
+     */
+    public static boolean inventoryContainsSpellName(@NotNull Inventory inv, @Nullable String spellName) {
+        if (spellName == null)
+            return false;
+
+        for (ItemStack item : inv.getContents())
+            if (SpellHandler.itemIsSpell(item) && spellName.equals(ItemData.getPersistentDataValue(item, PersistentDataKeys.spellNameKey, PersistentDataType.STRING)))
+                return true;
+        return false;
+    }
+}
