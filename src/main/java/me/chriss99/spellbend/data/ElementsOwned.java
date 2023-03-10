34b3f99ea383b4b1a040d42696037c84d6c110eb
chriss99 <chrissblock99@gmail.com>
2023-03-10 21:29:49 +0100

    added playerOwnsPreviousSpellInElement() and playerOwnsSpellGenerally() as well as som documentation

--- src/main/java/me/chriss99/spellbend/data/ElementsOwned.java
+++ src/main/java/me/chriss99/spellbend/data/ElementsOwned.java
@@ -14,12 +14,14 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
 
 public class ElementsOwned {
     private static final Gson gson = SpellBend.getGson();
 
     private final Player player;
-    private final EnumMap<ElementEnum, ArrayList<SpellEnum>> elementsOwned;
+    private final EnumMap<ElementEnum, EnumSet<SpellEnum>> elementsOwned;
 
     public ElementsOwned(@NotNull Player player) {
         this.player = player;
@@ -41,11 +43,40 @@ public class ElementsOwned {
         return elementsOwned.containsKey(elementEnum);
     }
 
-    public boolean playerOwnsSpell(@NotNull ElementEnum elementEnum, @NotNull SpellEnum spellEnum) {
-        ArrayList<SpellEnum> spellsOwned = elementsOwned.get(elementEnum);
+    /**
+     * Checks if the spell is contained in the set under that element
+     *
+     * @param elementEnum The element to check in
+     * @param spellEnum The spell to check for
+     * @return If the player owns the spell in that element
+     */
+    public boolean playerOwnsSpellInElement(@NotNull ElementEnum elementEnum, @NotNull SpellEnum spellEnum) {
+        EnumSet<SpellEnum> spellsOwned = elementsOwned.get(elementEnum);
         if (spellsOwned == null)
             return false;
         return spellsOwned.contains(spellEnum);
+    }
+
+    /**
+     * Checks if the spell is contained in any of the sets in the map
+     *
+     * @param spellEnum The spell to check for
+     * @return If the player owns that spell anywhere
+     */
+    public boolean playerOwnsSpellGenerally(@NotNull SpellEnum spellEnum) {
+        for (Map.Entry<ElementEnum, EnumSet<SpellEnum>> entry : elementsOwned.entrySet())
+            if (entry.getValue().contains(spellEnum))
+                return true;
+        return false;
+    }
+
+    public boolean playerOwnsPreviousSpellInElement(@NotNull ElementEnum elementEnum, @NotNull SpellEnum spellEnum) {
+        int index = elementEnum.getSpells().indexOf(spellEnum)-1;
+        if (index == -2) {
+            Bukkit.getLogger().warning("It was queried if " + player.getName() + " owns " + spellEnum + " in " + elementEnum + " which is not contained in that element!");
+            return false;
+        }
+        return playerOwnsSpellInElement(elementEnum, elementEnum.getSpell(index));
     }
 
     public Player getPlayer() {
