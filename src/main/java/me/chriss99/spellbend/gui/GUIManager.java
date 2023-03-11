90b4ceb508e1b14e2506639f0d0bea9ac7739f3b
chriss99 <chrissblock99@gmail.com>
2023-03-11 18:58:21 +0100

    Revert "rough shop layout"
    This reverts commit e019abe5d1db1be20d6a02cb321844458817b727.

--- src/main/java/me/chriss99/spellbend/gui/GUIManager.java
+++ src/main/java/me/chriss99/spellbend/gui/GUIManager.java
@@ -1,29 +1,18 @@
 package me.chriss99.spellbend.gui;
 
-import org.bukkit.event.inventory.InventoryEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class GUIManager {
-    /**
-     * A list of all Functions that convert an InventoryEvent to a comparable ArrayList of Objects <br>
-     * These return null if the InventoryEvent is not applicable or similar
-     */
-    private static final LinkedList<EventProcessor> eventProcessors = new LinkedList<>();
+    private static final LinkedHashMap<Function<ItemStack, Boolean>, Consumer<InventoryClickEvent>> inventoryClickEventConsumers = new LinkedList<>();
 
-    public static void onInventoryEvent(@NotNull InventoryEvent event) {
-        for (EventProcessor eventProcessor : eventProcessors)
-            eventProcessor.process(event);
-    }
+    private static void itemClickEvent(@NotNull InventoryClickEvent event) {
 
-    public static void registerEventProcessor(@NotNull Function<InventoryEvent, ArrayList<Object>> eventAdapter, @NotNull HashMap<ArrayList<Object>, Consumer<InventoryEvent>> comparableEventToConsumerMap) {
-        eventProcessors.add(new EventProcessor(eventAdapter, comparableEventToConsumerMap));
-    }
-
-    public static void registerEventProcessor(@NotNull EventProcessor eventProcessor) {
-        eventProcessors.add(eventProcessor);
     }
 }
