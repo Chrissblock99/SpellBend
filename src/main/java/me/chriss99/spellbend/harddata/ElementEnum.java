2d3ec5a2736cbba3dcca328c081febe0a9c164f2
chriss99 <chrissblock99@gmail.com>
2023-03-13 20:03:06 +0100

    added names to element display items

--- src/main/java/me/chriss99/spellbend/harddata/ElementEnum.java
+++ src/main/java/me/chriss99/spellbend/harddata/ElementEnum.java
@@ -1,6 +1,7 @@
 package me.chriss99.spellbend.harddata;
 
 import me.chriss99.spellbend.data.PlayerSessionData;
+import me.chriss99.spellbend.util.Item;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -10,17 +11,28 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public enum ElementEnum {
-    EMBER(new ItemStack(Material.FIRE_CHARGE), new SpellEnum[]{SpellEnum.MAGMA_BURST, SpellEnum.EMBER_BLAST, SpellEnum.SCORCHING_COLUMN, SpellEnum.BLAZING_SPIN, SpellEnum.FIERY_RAGE}, 150),
-    WATER(new ItemStack(Material.WATER_BUCKET), new SpellEnum[]{SpellEnum.HYDRO_BLAST, SpellEnum.WATER_SPRAY, SpellEnum.WATER_TORRENT, SpellEnum.RISING_TIDE, SpellEnum.SEA_SHIELD}, 150),
-    NATURE(new ItemStack(Material.OAK_SAPLING), new SpellEnum[]{SpellEnum.VERDANT_SPORES, SpellEnum.POISON_DARTS, SpellEnum.AUTUMN_WINDS, SpellEnum.VINE_GRAB, SpellEnum.NATURES_AEGIS}, 150),
-    EARTH(new ItemStack(Material.COARSE_DIRT), new SpellEnum[]{SpellEnum.SERRATED_EARTH, SpellEnum.LANDSLIDE, SpellEnum.BURROW, SpellEnum.SEISMIC_SMASH, SpellEnum.ROCK_BODY}, 300),
-    ELECTRO(new ItemStack(Material.BEACON), new SpellEnum[]{SpellEnum.LIGHTNING_BOLT, SpellEnum.FLASH, SpellEnum.SEISMIC_SHOCK, SpellEnum.LIGHTNING_CHAIN, SpellEnum.GALVANISE}, 300),
-    ICE(new ItemStack(Material.PACKED_ICE), new SpellEnum[]{SpellEnum.TWISTED_FLURRY, SpellEnum.FROSTBITE, SpellEnum.WINTERS_HEAVE, SpellEnum.AVALANCHE, SpellEnum.GLACIAL_ARMAMENT}, 300),
-    AETHER(new ItemStack(Material.CONDUIT), new SpellEnum[]{SpellEnum.METEOR_BELT, SpellEnum.BLACK_HOLE, SpellEnum.STELLAR_SLAM, SpellEnum.AETHERS_WRATH, SpellEnum.COSMIC_SMASH}, 650),
-    SOUL(new ItemStack(Material.SKELETON_SKULL), new SpellEnum[]{SpellEnum.SCATTER_BONES, SpellEnum.SOUL_DRAIN, SpellEnum.SEEKING_SKULL, SpellEnum.GASHING_FOSSILS, SpellEnum.PHANTOMS_CURSE}, 650),
-    TIME(new ItemStack(Material.CLOCK), new SpellEnum[]{SpellEnum.DARTS_OF_TIME, SpellEnum.ESCAPE_THROUGH_TIME, SpellEnum.DEATHLY_HOUR, SpellEnum.CHRONOPUNCH, SpellEnum.TEMPORAL_ILLUSION}, 650),
-    METAL(new ItemStack(Material.IRON_INGOT), new SpellEnum[]{SpellEnum.CATAPULT, SpellEnum.ALLOYED_BARRIER, SpellEnum.METARANG, SpellEnum.RAZOR_SPIN, SpellEnum.ANCIENT_TEMPER}, 900),
-    EXPLOSION(new ItemStack(Material.TNT), new SpellEnum[]{SpellEnum.FIERY_MISSILE, SpellEnum.LANDMINES, SpellEnum.BLAST_OFF, SpellEnum.FLYING_BLITZ, SpellEnum.BOMB_HEAD}, 900);
+    EMBER(Item.create(Material.FIRE_CHARGE, "<red><bold>Ember"),
+            new SpellEnum[]{SpellEnum.MAGMA_BURST, SpellEnum.EMBER_BLAST, SpellEnum.SCORCHING_COLUMN, SpellEnum.BLAZING_SPIN, SpellEnum.FIERY_RAGE}, 150),
+    WATER(Item.create(Material.WATER_BUCKET, "<blue><bold>Water"),
+            new SpellEnum[]{SpellEnum.HYDRO_BLAST, SpellEnum.WATER_SPRAY, SpellEnum.WATER_TORRENT, SpellEnum.RISING_TIDE, SpellEnum.SEA_SHIELD}, 150),
+    NATURE(Item.create(Material.OAK_SAPLING, "<green><bold>Nature"),
+            new SpellEnum[]{SpellEnum.VERDANT_SPORES, SpellEnum.POISON_DARTS, SpellEnum.AUTUMN_WINDS, SpellEnum.VINE_GRAB, SpellEnum.NATURES_AEGIS}, 150),
+    EARTH(Item.create(Material.COARSE_DIRT, "<dark_green><bold>Earth"),
+            new SpellEnum[]{SpellEnum.SERRATED_EARTH, SpellEnum.LANDSLIDE, SpellEnum.BURROW, SpellEnum.SEISMIC_SMASH, SpellEnum.ROCK_BODY}, 300),
+    ELECTRO(Item.create(Material.BEACON, "<aqua><bold>Electro"),
+            new SpellEnum[]{SpellEnum.LIGHTNING_BOLT, SpellEnum.FLASH, SpellEnum.SEISMIC_SHOCK, SpellEnum.LIGHTNING_CHAIN, SpellEnum.GALVANISE}, 300),
+    ICE(Item.create(Material.PACKED_ICE, "<white><bold>Ice"),
+            new SpellEnum[]{SpellEnum.TWISTED_FLURRY, SpellEnum.FROSTBITE, SpellEnum.WINTERS_HEAVE, SpellEnum.AVALANCHE, SpellEnum.GLACIAL_ARMAMENT}, 300),
+    AETHER(Item.create(Material.CONDUIT, "<gray><bold>Aether"),
+            new SpellEnum[]{SpellEnum.METEOR_BELT, SpellEnum.BLACK_HOLE, SpellEnum.STELLAR_SLAM, SpellEnum.AETHERS_WRATH, SpellEnum.COSMIC_SMASH}, 650),
+    SOUL(Item.create(Material.SKELETON_SKULL, "<dark_purple><bold>Soul"),
+            new SpellEnum[]{SpellEnum.SCATTER_BONES, SpellEnum.SOUL_DRAIN, SpellEnum.SEEKING_SKULL, SpellEnum.GASHING_FOSSILS, SpellEnum.PHANTOMS_CURSE}, 650),
+    TIME(Item.create(Material.CLOCK, "<yellow><bold>Time"),
+            new SpellEnum[]{SpellEnum.DARTS_OF_TIME, SpellEnum.ESCAPE_THROUGH_TIME, SpellEnum.DEATHLY_HOUR, SpellEnum.CHRONOPUNCH, SpellEnum.TEMPORAL_ILLUSION}, 650),
+    METAL(Item.create(Material.IRON_INGOT, "<dark_gray><bold>Metal"),
+            new SpellEnum[]{SpellEnum.CATAPULT, SpellEnum.ALLOYED_BARRIER, SpellEnum.METARANG, SpellEnum.RAZOR_SPIN, SpellEnum.ANCIENT_TEMPER}, 900),
+    EXPLOSION(Item.create(Material.TNT, "<gold><bold>Explosion"),
+            new SpellEnum[]{SpellEnum.FIERY_MISSILE, SpellEnum.LANDMINES, SpellEnum.BLAST_OFF, SpellEnum.FLYING_BLITZ, SpellEnum.BOMB_HEAD}, 900);
 
     private final ItemStack displayItem;
     private final List<SpellEnum> spells;
