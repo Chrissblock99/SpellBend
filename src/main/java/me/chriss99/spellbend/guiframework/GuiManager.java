f3b4bad664686bdb8c6839d1690ab84876077fec
chriss99 <chrissblock99@gmail.com>
2023-03-14 21:50:23 +0100

    the Gui framework now calls functions on any click related to a gui

--- src/main/java/me/chriss99/spellbend/guiframework/GuiManager.java
+++ src/main/java/me/chriss99/spellbend/guiframework/GuiManager.java
@@ -2,6 +2,7 @@ package me.chriss99.spellbend.guiframework;
 
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -15,10 +16,26 @@ public class GuiManager {
 
     public static void click(@NotNull InventoryClickEvent clickEvent) {
         GuiInventory guiInventory = guiInventories.get(clickEvent.getClickedInventory());
+        if (guiInventory == null) {
+            nonGuiClicked(clickEvent);
+            return;
+        }
+
+        guiInventory.click(clickEvent);
+    }
+
+    private static void nonGuiClicked(@NotNull InventoryClickEvent clickEvent) {
+        GuiInventory guiInventory = guiInventories.get(getNotClickedInventory(clickEvent));
         if (guiInventory == null)
             return;
 
-        clickEvent.setCancelled(true);
-        guiInventory.click(clickEvent);
+        guiInventory.clickInOtherInventory(clickEvent);
+    }
+
+    private static Inventory getNotClickedInventory(@NotNull InventoryClickEvent clickEvent) {
+        InventoryView inventoryView = clickEvent.getView();
+        if (inventoryView.getTopInventory().equals(clickEvent.getClickedInventory()))
+            return inventoryView.getBottomInventory();
+        return inventoryView.getTopInventory();
     }
 }
