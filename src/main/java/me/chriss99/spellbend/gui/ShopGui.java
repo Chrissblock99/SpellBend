0c582e846b7eca6e597ee9a9c97a9a65264eb98d
chriss99 <chrissblock99@gmail.com>
2023-03-15 12:44:00 +0100

    added sound and fixed coloring issues

--- src/main/java/me/chriss99/spellbend/gui/ShopGui.java
+++ src/main/java/me/chriss99/spellbend/gui/ShopGui.java
@@ -16,13 +16,17 @@ import me.chriss99.spellbend.util.Item;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 public class ShopGui extends GuiInventory {
+    private static final SpellBend plugin = SpellBend.getInstance();
     private static final MiniMessage miniMessage = SpellBend.getMiniMessage();
     private static final ElementEnum[] elementEnums = ElementEnum.values();
 
@@ -50,6 +54,13 @@ public class ShopGui extends GuiInventory {
         }
 
         player.openInventory(inventory);
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 10f, 1.2f);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 10f, 1.5f);
+            }
+        }.runTaskLater(plugin, 4);
     }
 
     @Override
@@ -64,16 +75,26 @@ public class ShopGui extends GuiInventory {
 
     public static void shopClick(@NotNull InventoryClickEvent event) {
         event.setCancelled(true);
-        if (SpellHandler.itemIsSpell(event.getCursor()))
+        if (SpellHandler.itemIsSpell(event.getCursor())) {
+            unequipMessage((Player) event.getWhoClicked(), event.getCursor());
             event.setCursor(null);
+        }
     }
 
     public static void nonShopClick(@NotNull InventoryClickEvent event) {
         if (event.getClick().isShiftClick()) {
             event.setCancelled(true);
-            if (SpellHandler.itemIsSpell(event.getCurrentItem()))
+            if (SpellHandler.itemIsSpell(event.getCurrentItem())) {
+                unequipMessage((Player) event.getWhoClicked(), event.getCurrentItem());
                 event.setCurrentItem(null);
+            }
         }
+    }
+
+    public static void unequipMessage(@NotNull Player player, @NotNull ItemStack item) {
+        player.sendMessage(miniMessage.deserialize("<blue><bold>SHOP <reset><dark_gray>» <yellow>Unequipped " + miniMessage.serializeOrNull(item.getItemMeta().displayName()) +
+                "<reset><yellow>!"));
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 10f, 0.8f);
     }
 
     private static void elementClickEvent(@NotNull InventoryClickEvent clickEvent, @NotNull Player player, @NotNull PlayerSessionData sessionData, @NotNull ElementEnum elementEnum) {
@@ -89,7 +110,9 @@ public class ShopGui extends GuiInventory {
     private static void elementShiftClick(@NotNull Player player, @NotNull PlayerSessionData sessionData, @NotNull ElementEnum elementEnum) {
         ElementsOwned elementsOwned = sessionData.getElementsOwned();
         if (!elementsOwned.playerOwnsElement(elementEnum)) {
-            player.sendMessage(miniMessage.deserialize("<blue><bold>SHOP <dark_gray>» <red>You do not own " + elementEnum + "<red>!"));
+            player.sendMessage(miniMessage.deserialize("<blue><bold>SHOP <reset><dark_gray>» <red>You do not own " +
+                    miniMessage.serializeOrNull(elementEnum.getDisplayItem().getItemMeta().displayName()) + "<reset><red>!"));
+            player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 10f, 0.8f);
             return;
         }
 
@@ -99,23 +122,25 @@ public class ShopGui extends GuiInventory {
     private static void elementClick(@NotNull Player player, @NotNull PlayerSessionData sessionData, @NotNull ElementEnum elementEnum) {
         ElementsOwned elementsOwned = sessionData.getElementsOwned();
         if (elementsOwned.playerOwnsElement(elementEnum)) {
-            new ElementGui(player, elementEnum);
+            ElementGui.open(player, elementEnum, false);
             return;
         }
 
         CurrencyTracker gems = sessionData.getGems();
         if (!elementEnum.playerCanBuy(sessionData)) {
-            player.sendMessage(miniMessage.deserialize("<blue><bold>SHOP <dark_gray>»<red> Not enough Gems! Need <aqua>" +
+            player.sendMessage(miniMessage.deserialize("<blue><bold>SHOP <reset><dark_gray>»<red> Not enough Gems! Need <aqua>" +
                     (long) (elementEnum.getPrice() - gems.getCurrency()) + "<red> more!"));
+            player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 10f, 0.8f);
             return;
         }
 
         gems.addCurrency(-elementEnum.getPrice());
         elementsOwned.setElementOwned(elementEnum);
-        new ElementGui(player, elementEnum);
-        player.sendMessage(miniMessage.deserialize("<blue><bold>SHOP <dark_gray>»<yellow> Purchased " +
-                miniMessage.serializeOrNull(elementEnum.getDisplayItem().getItemMeta().displayName()) + " <gold>for <aqua>" + elementEnum.getPrice() + " Gems<yellow>!"));
-        //TODO buy sound here
+        ElementGui.open(player, elementEnum, true);
+        player.sendMessage(miniMessage.deserialize("<blue><bold>SHOP <reset><dark_gray>»<yellow> Purchased " +
+                miniMessage.serializeOrNull(elementEnum.getDisplayItem().getItemMeta().displayName()) + " <reset><gold>for <aqua>" + elementEnum.getPrice() + " Gems<yellow>!"));
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 10f, 1.2f);
+        player.playSound(player.getLocation(), Sound.ITEM_TOTEM_USE, 0.5f, 1.5f);
     }
 
     public static void addAllElementSpells(@NotNull Player player, @NotNull ElementsOwned elementsOwned, @NotNull ElementEnum elementEnum) {
@@ -130,7 +155,7 @@ public class ShopGui extends GuiInventory {
 
     private static @NotNull String[] createElementOwningLore(@NotNull PlayerSessionData sessionData, @NotNull ElementEnum elementEnum) {
         if (sessionData.getElementsOwned().playerOwnsElement(elementEnum))
-            return new String[]{"<dark_gray>----------------", "<yellow><bold>SHIFT CLICK TO EQUIP"};
+            return new String[]{"<dark_gray>----------------", "<green><bold>SHIFT CLICK TO EQUIP"};
 
         return new String[]{"<dark_gray>----------------", "<green>$ <aqua>" + elementEnum.getPrice() + " <dark_aqua>Gems",
                 (elementEnum.playerCanBuy(sessionData)) ? "<gold>You can<yellow> buy<gold> this!" : "<red>You can't<yellow> buy<red> this yet!"};
