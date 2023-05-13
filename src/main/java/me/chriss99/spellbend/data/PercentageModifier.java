53f3ef748731cbd96398e65c128e9d35d7083e4c
chriss99 <chrissblock99@gmail.com>
2023-05-13 22:03:34 +0200

    PercentageModifier now uses Percentage.java internally

--- src/main/java/me/chriss99/spellbend/data/PercentageModifier.java
+++ src/main/java/me/chriss99/spellbend/data/PercentageModifier.java
@@ -3,6 +3,7 @@ package me.chriss99.spellbend.data;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import me.chriss99.spellbend.SpellBend;
+import me.chriss99.spellbend.util.math.Percentage;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.LivingEntity;
@@ -15,9 +16,8 @@ public class PercentageModifier {
     private static final Gson gson = SpellBend.getGson();
 
     private final LivingEntity livingEntity;
-    private double modifier;
-    private int isZero;
     private final NamespacedKey key;
+    private final Percentage percentage;
 
     public PercentageModifier(@NotNull LivingEntity livingEntity, @NotNull NamespacedKey key, @NotNull String name) {
         this.livingEntity = livingEntity;
@@ -27,19 +27,15 @@ public class PercentageModifier {
 
         if (gsonString == null) {
             Bukkit.getLogger().warning(livingEntity.getName() + "'s " + name + " were not setup when loading, fixing now!");
-            modifier = 1;
-            isZero = 0;
-            livingEntity.getPersistentDataContainer().set(key, PersistentDataType.STRING, gson.toJson(getDefaultData()));
+            percentage = new Percentage(1, 0);
+            livingEntity.getPersistentDataContainer().set(key, PersistentDataType.STRING, gson.toJson(percentage));
             return;
         }
 
-        Type type = new TypeToken<Data>(){}.getType();
-        Data data = gson.fromJson(gsonString, type);
-        modifier = data.modifier;
-        if (modifier == 0)
+        percentage = gson.fromJson(gsonString, Percentage.class);
+        if (percentage.getRealPart() == 0)
             Bukkit.getLogger().warning("PercentageModifier for livingEntity " + livingEntity.getName() + " with key " + key.getKey() +
-                    " and name \"" + name + "\" loaded with a modifier of 0! This presents a problem as this value can never change!");
-        isZero = data.isZero;
+                    " and name \"" + name + "\" loaded with a real part of 0! This presents a problem as this value can never change!");
     }
 
     /**
@@ -48,12 +44,7 @@ public class PercentageModifier {
      * @return The modifier
      */
     public double getModifier() {
-        if (isZero > 0)
-            return 0;
-        else if (isZero < 0)
-            return modifier / 0;
-
-        return modifier;
+        return percentage.getPercentage();
     }
 
     /**
@@ -62,49 +53,29 @@ public class PercentageModifier {
      * therefore it can be undone by dividing it by the same modifier
      * To do that call removeModifier()
      *
-     * @throws IllegalArgumentException If the modifier is negative
-     *
-     * @param modifier The modifier not negative
+     * @param modifier The modifier
      */
     public void addModifier(double modifier) {
-        if (modifier < 0)
-            throw new IllegalArgumentException("Modifier cannot be negative!");
-        if (modifier == 1)
-            return;
-
-        if (this.modifier * modifier == 0) {
-            isZero++;
-            return;
-        }
-
-        this.modifier *= modifier;
+        percentage.multiply(modifier);
     }
 
     /**
      * Removes the modifier from the livingEntity
      * It does this by dividing the number with the modifier, undoing addModifier() in the process
      *
-     * @throws IllegalArgumentException If the modifier is negative
-     *
-     * @param modifier The modifier not negative
+     * @param modifier The modifier
      */
     public void removeModifier(double modifier) {
-        if (modifier < 0)
-            throw new IllegalArgumentException("Modifier cannot be negative!");
-        if (modifier == 1)
-            return;
+        percentage.divide(modifier);
+    }
 
-        if (modifier == 0) {
-            isZero--;
-            return;
-        }
-
-        if (this.modifier / modifier == 0) {
-            isZero++;
-            return;
-        }
-
-        this.modifier /= modifier;
+    /**
+     * Gets the internal percentage instance
+     *
+     * @return The internal percentage instance
+     */
+    public @NotNull Percentage getPercentage() {
+        return percentage;
     }
 
     public LivingEntity getLivingEntity() {
@@ -115,22 +86,6 @@ public class PercentageModifier {
      * Saves the livingEntities modifiers to their PersistentDataContainer
      */
     public void saveModifiers() {
-        livingEntity.getPersistentDataContainer().set(key, PersistentDataType.STRING,
-                gson.toJson(new Data(modifier, isZero)));
-    }
-
-    public static Data getDefaultData() {
-        return new Data(1, 0);
-    }
-
-    @SuppressWarnings("ClassCanBeRecord") //GSON will break otherwise
-    private static class Data {
-        public final double modifier;
-        public final int isZero;
-
-        public Data(double modifier, int isZero) {
-            this.modifier = modifier;
-            this.isZero = isZero;
-        }
+        livingEntity.getPersistentDataContainer().set(key, PersistentDataType.STRING, gson.toJson(percentage));
     }
 }
