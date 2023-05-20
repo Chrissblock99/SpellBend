634bedff7a6b9b840d1a391c2c9e9341081be509
Bellspend <118931329+Bellspend@users.noreply.github.com>
2023-05-20 15:59:14 -0400

    Update Rock_Body.java

--- src/main/java/me/chriss99/spellbend/spells/Rock_Body.java
+++ src/main/java/me/chriss99/spellbend/spells/Rock_Body.java
@@ -32,8 +32,8 @@ public class Rock_Body extends Spell {
     
     private void windup() {
         World world = caster.getWorld();
-        double vector = MathUtil.DEGTORAD(caster.getLocation().getYaw()+90);
-        for(int i = 0, i < 20, i++){
+        double vector = MathUtil.DEGTORAD * (caster.getLocation().getYaw()+90);
+        for (int i = 0, i < 20, i++) {
             FallingBlock lodestone = world.spawnFallingBlock(location,
         
         }                                                     
