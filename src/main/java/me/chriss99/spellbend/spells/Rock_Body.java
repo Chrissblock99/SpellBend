5da3aec70a12d5f332e229afac284ee1e8c17c3a
Bellspend <118931329+Bellspend@users.noreply.github.com>
2023-05-20 15:22:05 -0400

    Update Rock_Body.java

--- src/main/java/me/chriss99/spellbend/spells/Rock_Body.java
+++ src/main/java/me/chriss99/spellbend/spells/Rock_Body.java
@@ -1,9 +1,28 @@
 package me.chriss99.spellbend.spells;
 
+import me.chriss99.spellbend.SpellBend;
+import me.chriss99.spellbend.data.LivingEntitySessionData;
 import me.chriss99.spellbend.data.PlayerSessionData;
+import me.chriss99.spellbend.data.SpellHandler;
+import me.chriss99.spellbend.harddata.CoolDownStage;
+import me.chriss99.spellbend.manager.BlockManager;
+import me.chriss99.spellbend.util.LivingEntityUtil;
+import me.chriss99.spellbend.util.math.MathUtil;
+import org.bukkit.*;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class Rock_Body extends Spell {
     public Test_Spell(@NotNull Player caster, @NotNull String spellType, @NotNull ItemStack item) {
@@ -13,8 +32,9 @@ public class Rock_Body extends Spell {
     
     private void windup() {
         World world = caster.getWorld()
-        MathUtil.DEGTORADcaster.getLocation().getYaw()+90
+        double vector = MathUtil.DEGTORAD(caster.getLocation().getYaw())+90
             for(int i=0, i<=20, i++);
+            FallingBlock lodestones = world.spawnFallingBlock(location,  
                   
                 
 
